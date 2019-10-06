7c36aba1eaffd08a8f3f496acb9e450d4ca0612e
Thiagodoy <thiagodoy@hotmail.com>
2019-10-06 08:37:56 -0300

    Criação da api que realiza o o geocode para os endereços carregados

---
+++ click-top-front/src/services/google-geocode.js
@@ -0,0 +1,38 @@
+import Service from './service';
+
+class GoogleService extends Service {
+    constructor(url) {
+        super(url);
+    }
+
+    /**
+     * @author Thiago Godoy
+     * @summary Get location with address
+     * @param {Object} request
+     * @memberof GoogleService
+     */
+    getLocation(request) {
+
+        request = this.putKey(request);
+        return this.get(request, true).then(response=>{
+
+            if(response.results.length > 0){
+                return response.results[0].geometry.location;
+            }else{
+                throw new Error("Nenhuma referencia encontrada!");
+            }
+        });
+
+
+    }
+
+    putKey(request){
+        request.key = process.env.API_SERVICE_GOOGLE_KEY;
+        return request;
+    }
+
+
+}
+
+const instance = new GoogleService(process.env.API_SERVICE_GOOGLE);
+export default instance;
---
+++ click-top-front/src/services/service.js
@@ -0,0 +1,98 @@
+import axios from 'axios';
+//import Interceptor from './inteceptors';
+export default class Service {
+    constructor(url) {
+
+        this._api = axios.create({
+            baseURL: `${process.env.VUE_APP_BASE_PATH}${url}`,
+            timeout: 100000,
+            headers: { 'Access-Control-Allow-Origin': '*' }
+        });
+        //this._api.interceptors.response.use(...Interceptor.configure());
+        this._url = '';
+    }
+
+    /**
+     * @param  {} request
+     */
+    createQueryParams(request) {
+
+        if (!request) return;
+
+        let keys = Object.keys(request);
+        this._url += '?';
+        let count = 0
+        for (let key of keys) {
+            if (request[key] == null || request[key] === undefined || request[key] === 'undefined') {
+                continue;
+            }
+
+            if (Array.isArray(request[key])) {
+
+                this._url += (count > 0 ? '&' : '') + request[key].map(e => `${key}${encodeURIComponent('[]')}=${e}`).join('&');
+                count++;
+                continue;
+            }
+
+            this._url += `${((count > 0) ? '&' : '')}${key}=${request[key]}`;
+            count++;
+        }
+    }
+
+    /**
+     * @param  {} request
+     */
+    replaceParameters(request) {
+
+        let keys = Object.keys(request);
+        for (let key of keys) {
+            this._url = this._url.replace(`\{${key}\}`, request[key]);
+        }
+
+    }
+
+    /**
+     */
+    hasParameters() {
+        return /(\{[a-zA-Z]\})+/.test(this._url);
+    }
+
+    /**
+     * @param  {} request=null
+     */
+    get(request = null, queryParam = false) {
+
+
+
+        if (this.hasParameters() && !queryParam) {
+            this.replaceParameters(request);
+        } else if (queryParam) {
+            this.createQueryParams(request);
+        }
+
+        return this._api.get(this._url);
+    }
+
+    /**
+     * @param  {} request
+     */
+    post(request) {
+        return this._api.post(this._url, request);
+    }
+
+    /**
+     * @param  {} request
+     */
+    put(request) {
+        return this._api.put(this._url, request);
+    }
+
+    /**
+     * @param  {} request
+     */
+    delete(request) {
+        this.replaceParameters(request);
+        return this._api.delete(this._url);
+    }
+
+}
