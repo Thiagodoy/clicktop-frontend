f150902902c41b3103d9138352136e050a1939ed
brngomes90@gmail.com <brngomes90@gmail.com>
2019-10-09 01:13:15 -0300

    arquivos scss, paginas, layout

---
+++ click-top-front/src/layout/app.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <section class="section">
+      <router-view></router-view>
+    </section>
+    <app-footer></app-footer>
+    <!-- <message-dialog></message-dialog> -->
+  </div>
+</template>
+<script>
+import AppHeader from './app-header.vue'
+import AppFooter from './app-footer.vue'
+
+export default {
+  created(){
+    // Detecta o user-agent e coloca classe correspondento no document
+    (function(d, l) {
+      for (var k in l) {
+        if(navigator.userAgent.match(new RegExp(l[k], 'i'))){
+          d[k] = true;
+          d.documentElement.className+=' '+k;
+        }
+      }
+    }(document, {
+      ios: '(iPhone|iPod|iPad)',
+      android: 'android',
+      mobile: 'mobile',
+      webview: '((iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)|webview)'
+    }));
+
+  },
+  mounted(){
+    //$('body').append(document.getElementsByTagName('html')[0].classList.value);
+    //$('body').append('<br>'+navigator.userAgent);
+
+  },
+  components: {
+      AppHeader, AppFooter
+  },
+
+}
+</script>
---
+++ click-top-front/src/pages/account/register.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="container">
+      <form class="" action="index.html" method="post">
+        <div class="row">
+          <div class="col-12">
+            <div class="form-group">
+              <label for="">Nome</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+            <div class="form-group">
+              <label for="">Telefone</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+            <div class="form-group">
+              <label for="">Endereço</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+            <div class="form-group">
+              <label for="">Localização geográfica</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+            <div class="form-group">
+              <label for="">Empresa</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+            <div class="form-group">
+              <label for="">Horário de funcionamento</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+            <div class="form-group">
+              <label for="">E-mail para contato (orçamento)</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+            <div class="form-group">
+              <label for="">Palavras chave para busca</label>
+              <textarea class="form-control" name="name" rows="8" cols="80"></textarea>
+            </div>
+            <div class="form-group">
+              <label for="">Missão, visão e valores</label>
+              <textarea class="form-control" name="name" rows="8" cols="80"></textarea>
+            </div>
+            <div class="form-group">
+              <label for="">Principais produtos</label>
+              <textarea class="form-control" name="name" rows="8" cols="80"></textarea>
+            </div>
+            <div class="form-group">
+              <label for="">Site</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+            <div class="form-group">
+              <label for="">Facebook</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+            <div class="form-group">
+              <label for="">Instagram</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+            <div class="form-group">
+              <label for="">Link do Whats</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+            <div class="form-group">
+              <label for="">Fotos</label>
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+              </div>
+              <div class="custom-file">
+                <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+                <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
