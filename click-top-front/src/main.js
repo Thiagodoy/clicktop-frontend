2f7a50cf8883c60d7f1bb3f6621921f1d4cfe3f0
Thiagodoy <thiagodoy@hotmail.com>
2019-10-08 21:41:33 -0300

    Criação do loader scss

--- click-top-front/src/main.js
+++ click-top-front/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './routes/index';
+import store from './store'
 
 Vue.config.productionTip = false
 
-new Vue({  
+new Vue({
   router,
-  render: h => h(App),
-
+  store,
+  render: h => h(App)
 }).$mount('#app')
