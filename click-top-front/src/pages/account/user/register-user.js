743f7a3dac3b5f1ec0197d8adf1d35b88209f8e0
brngomes90@gmail.com <brngomes90@gmail.com>
2019-10-31 22:36:11 -0300

    usuario

---
+++ click-top-front/src/pages/account/user/register-user.js
@@ -0,0 +1,40 @@
+import Company from '../../../services/user'
+
+export default {
+  data() {
+    return {
+      user: {
+        // photo: undefined,
+        // name: undefined,
+        // address: undefined,
+        // city: undefined,
+        // state: undefined,
+        // geo: undefined,
+        // email: undefined,
+        // site: undefined,
+        // phone: undefined,
+        // hour: undefined,
+        // keyword: undefined,
+        // mission: undefined,
+        // products: undefined,
+        // face: undefined,
+        // insta: undefined,
+        // whats: undefined
+      },
+      loading: false
+    }
+  },
+  methods: {
+    saveCompany() {
+      console.log('oi');
+      Company.saveUser(this.company).then(resp => {
+        console.log('oioioioi');
+        // auth.updateLocalStorage();
+        console.log(resp, ' resp')
+      })
+      .catch(err =>
+        console.log(err, ' err')
+      )
+    },
+  }
+}
---
+++ click-top-front/src/pages/account/user/register-user.vue
@@ -0,0 +1,58 @@
+<template id="">
+  <div class="">
+    <div class="container">
+      <form class="" action="index.html" method="post">
+        <h1 class="up">inserir usuário</h1>
+        <div class="row">
+          <div class="col-3">
+            <div class="form-group">
+              <div class="custom-file">
+                <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+                <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+              </div>
+            </div>
+          </div>
+          <div class="col-9">
+            <div class="form-group">
+              <label for="">Nome</label>
+              <input class="form-control" type="text" name="" value="" v-model="user.name">
+            </div>
+            <div class="form-group">
+              <label for="">Email</label>
+              <input class="form-control" type="email" name="" value="" v-model="user.email">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="form-group">
+                <label for="">Senha</label>
+                <input class="form-control" type="password" name="" value="" v-model="user.password">
+              </div>
+            </div>
+            <div class="col">
+              <div class="form-group">
+                <label for="">Confirmar senha</label>
+                <input class="form-control" type="password" name="" value="" v-model="user.repassword">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="form-group">
+                <label for="">Status</label>
+                <input class="form-control" type="password" name="" value="" v-model="user.status">
+              </div>
+            </div>
+            <div class="col">
+              <div class="form-group">
+                <label for="">Profile</label>
+                <input class="form-control" type="text" name="" value="" v-model="user.profile">
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script src="./register-user.js"></script>
